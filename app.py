8e396d0e5102e7046d5c6799396c0e821993207d
Khizar <khizar.naeem2010@gmail.com>
2016-10-19 21:29:42 +0200

    lets see the change

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-    return "Hello world", 200
+	log("data")
+
+    return "Hello worldss", 200
 
 
 @app.route('/', methods=['POST'])
